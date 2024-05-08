84429ab925d798701342bdacfc8867c786a350fa
Shinrael <mircoelena89@gmail.com>
2024-05-08 15:38:54 +0200

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
+    <title>laravel-primi-passi</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Hello World!</h1>
+        <h2>{{ $salutation }} {{ $name }} ma scritto in modo dinamico!</h2>
+    </div>
+</body>
+</html>
